f4efaad15ca5c99235798e416d010d9e0bc44302
Crowdbotics <team@crowdbotics.com>
2023-02-06 17:17:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/super_mud_38746/MainActivity.java
@@ -0,0 +1,15 @@
+package com.super_mud_38746;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "super_mud_38746";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-mud-38746",
+  url: "https://super-mud-38746.botics.co",
+  api: "https://super-mud-38746.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
